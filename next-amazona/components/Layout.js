214d6e8d0701321cbfba384637cb8f93bbed719e
lucadavid075 <daiveedlucas049@gmail.com>
2022-02-20 21:59:05 +0100

    Initialized Next in Repo

---
+++ next-amazona/components/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Layout() {
+  return (
+    <div>
+        <Head></Head>
+    </div>
+  )
+}
---
+++ next-amazona/pages/index.js
@@ -0,0 +1,16 @@
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </div>
+  )
+}
